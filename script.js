989accbd2f45b7faed386a6527e80aaf43d12d94
Bernie Petters <bp4924@gmail.com>
2023-03-03 08:57:01 -0500

    game functions

--- script.js
+++ script.js
@@ -8,6 +8,34 @@ const answerButtonsElement = document.getElementById("answer-button");
 let shuffledQuestions, currentQuestionIndex;
 let quizScore = 0;
 
+function startGame() {
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+  quizScore = 0;
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add(btn);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
 // reset
 function resetState() {
   clearStatusClass(document.body);
@@ -16,6 +44,7 @@ function resetState() {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
+
 // q&a logic
 function selectAnswer(e) {
   const selectedButton = e.target;
